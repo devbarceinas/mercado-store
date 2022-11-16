5495b4a3062a0c9e3e6ec89bae5f721e1d71323e
devmario <devmario@cexs-Mac-Pro.local>
2022-11-15 18:45:59 -0600

    feat(search): add styles the products search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/images/Logo_ML.png'
+import iconSearch from '../../assets/images/ic_Search.png'
 
 import './styles.scss'
 
@@ -6,6 +7,14 @@ const Search = () => {
   return (
     <nav className='navContainer'>
       <img src={logo} alt="Logo ML" />
+      <section className='inputContainer'>
+        <input 
+          type="text" 
+          placeholder='Nunca dejes de buscar' 
+          name='search'
+        />
+        <img src={iconSearch} alt="Icon Search" />
+      </section>
     </nav>
   )
 }
